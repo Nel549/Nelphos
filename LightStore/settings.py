5250ba9cddb0329ffb1e4c7468522f7736653ed6
Nelu <ionbelei549@gmail.com>
2022-11-04 23:31:42 +0200

    product2

--- LightStore/settings.py
+++ LightStore/settings.py
@@ -27,7 +27,7 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
